67a417c19dc8f69c71fd906a4319ffd4a6d17a71
Yogi sukma <yosukma257@gmail.com>
2022-11-25 18:10:02 +0700

    commit2

--- resources/views/pengaduan/index.blade.php
+++ resources/views/pengaduan/index.blade.php
@@ -11,13 +11,37 @@
     background-size: contain;
     background-repeat: no-repeat;
     height: 140vh;
-    opacity: 50%;
+    opacity: 90%;
 }    
 </style>  
 </head>
   @include('partial.navbar')
   <body>
-    <div class="background-image"></div>
+    <div class="background-image">
+        <br><br>
+        <div class="card mx-auto bg-secondary" style="opacity:70%; width: 30rem;">
+            <div class="mb-3 mx-auto" style="width: 80%">
+                <label for="formGroupExampleInput" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3 mx-auto" style="width: 80%">
+                <label for="formGroupExampleInput" class="form-label">Nomor HP</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3 mx-auto" style="width: 80%">
+                <label for="formGroupExampleInput" class="form-label">Email</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3 mx-auto" style="width: 80%">
+                <label for="formGroupExampleInput" class="form-label">Lokasi</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+            </div>
+            <div class="mb-3 mx-auto" style="width: 80%">
+                <label for="formGroupExampleInput" class="form-label">Isi Aduan</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
   @include('partial.footer')
