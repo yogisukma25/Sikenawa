9c5d0dc3a87afc5ec6319630cf4bdca615f7dbfb
Yogi sukma <yosukma257@gmail.com>
2022-11-25 17:35:36 +0700

    commit1

--- resources/views/pengaduan/index.blade.php
+++ resources/views/pengaduan/index.blade.php
@@ -5,11 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
+<style>
+.background-image{
+    background-image: url('https://cdn.pixabay.com/photo/2022/02/17/17/21/cows-7019167_1280.jpg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 140vh;
+    opacity: 50%;
+}    
+</style>  
+</head>
   @include('partial.navbar')
   <body>
-    <img class="fixed" src="https://cdn.pixabay.com/photo/2022/02/17/17/21/cows-7019167_1280.jpg" alt="">
-    
+    <div class="background-image"></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
   @include('partial.footer')
